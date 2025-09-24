a69712074175485fb28c23c740356b6da7513bdc
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2025-09-24 12:21:23 +0700

    first commit: add index, css, js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, semangat magang + ngerjain Tugas Akhirnya sayangggg\n\nMari kita mulai jalan - jalan dan jajan - jajan minggu depan lagii\n\nI just wonder… aku ngapain coba bikin kaya gini..\n\nAnyway, I hope you're doing okay, di mam pecelnyaaaa.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, kapan kita akan makan mie pangsit?\n\nKapan aku akan mendapat hidayah dalam judul skripsinya\n\nI just wonder… aku ngapain coba bikin kaya gini..\n\nAnyway, I hope you're doing okay, akhir kata, saya PULANG minggu depan`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
